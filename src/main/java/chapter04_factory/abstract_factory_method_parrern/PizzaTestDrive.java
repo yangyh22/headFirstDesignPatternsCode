1a97c815020cf99ac352d6c478b8979265fb9454
451561292@qq.com <451561292@qq.com>
2018-09-03 16:35:28 +0800

    抽象工厂

--- src/main/java/chapter04_factory/abstract_factory_method_parrern/PizzaTestDrive.java
+++ src/main/java/chapter04_factory/abstract_factory_method_parrern/PizzaTestDrive.java
@@ -1,8 +1,5 @@
 package chapter04_factory.abstract_factory_method_parrern;
 
-import chapter04_factory.abstract_factory_method_parrern.Ingredient.NYPizzaIngredientFactory;
-import chapter04_factory.abstract_factory_method_parrern.Ingredient.PizzaIngredientFactory;
-import chapter04_factory.abstract_factory_method_parrern.Pizza.CheesePizza;
 import chapter04_factory.abstract_factory_method_parrern.Pizza.NYPizzaStore;
 import chapter04_factory.abstract_factory_method_parrern.Pizza.Pizza;
 import chapter04_factory.abstract_factory_method_parrern.Pizza.PizzaStroe;
