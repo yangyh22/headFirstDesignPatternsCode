09359ad846ad902ab08b868630ef169796cbf2d0
451561292@qq.com <451561292@qq.com>
2018-09-03 14:38:09 +0800

    修改包名

---
+++ src/main/java/chapter04_factory/factory_method_parrern/Pizza.java
@@ -0,0 +1,43 @@
+package chapter04_factory.factory_method_parrern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yangyh
+ * @date 2018/9/3
+ */
+public abstract class Pizza {
+
+    String name;
+    String dough;
+    String sauce;
+    List<String> topings = new ArrayList<String>();
+
+    public void prepare() {
+        System.out.println("Preparing " + name);
+        System.out.println("Tossing dough...");
+        System.out.println("Adding sauce...");
+        System.out.println("Adding toppings...");
+        for (String toping : topings) {
+            System.out.println(toping);
+        }
+
+    }
+
+    public void bake() {
+        System.out.println("Bake for 25 minutes at 350");
+    }
+
+    public void cut() {
+        System.out.println("Cuuting the pizza into diagonal slices");
+    }
+
+    public void box() {
+        System.out.println("Pllace in official PizzaStore box");
+    }
+
+    // public Pizza(String name) {
+    //     this.name = name;
+    // }
+}
---
+++ src/main/java/chapter04_factory/factory_method_parrern/PizzaStroe.java
@@ -0,0 +1,26 @@
+package chapter04_factory.factory_method_parrern;
+
+
+/**
+ * @author yangyh
+ * @date 2018/9/3
+ */
+public abstract class PizzaStroe {
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza;
+
+        pizza = cratePizza(type);
+
+        pizza.prepare();
+        ;
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+
+    }
+
+    abstract Pizza cratePizza(String type);
+}
